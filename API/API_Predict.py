8c57a273e23e37c5b57f26861ca043d3fb53b3d4
Edward H.M <edwardhm865@gmail.com>
2020-05-05 00:09:19 +0700

    Semua optimasi ke Apps + API new route add

--- API/API_Predict.py
+++ API/API_Predict.py
@@ -83,7 +83,7 @@ def LeNet_Adam():
     label = "{}".format(label)
 
     print("hasil prediksi : " + label)
-    return label
+    return label + " (LeNet-Adam)"
 
 @app.route('/LeNet-Nadam', methods=['POST']) 
 def LeNet_Nadam():
@@ -148,7 +148,439 @@ def LeNet_Nadam():
     label = "{}".format(label)
 
     print("hasil prediksi : " + label)
-    return label
+    return label + " (LeNet-Nadam)"
+
+@app.route('/LeNet-SGD', methods=['POST']) 
+def LeNet_SGD():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(32,32))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_lenet(SGD).model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label + "(LeNet-SGD)"
+
+
+@app.route('/AlexNet-Adam', methods=['POST']) 
+def AlexNet_Adam():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(227,227))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_AlexNet.model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label+ " (AlexNet-Adam)"
+
+@app.route('/AlexNet-Nadam', methods=['POST']) 
+def AlexNet_Nadam():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(227,227))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_AlexNet(NADAM).model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label + " (AlexNet-Nadam)"
+
+
+@app.route('/AlexNet-SGD', methods=['POST']) 
+def AlexNet_SGD():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(227,227))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_AlexNet(SGD).model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label + " (AlexNet-SGD)"
+
+
+@app.route('/MiniVgg-Adam', methods=['POST']) 
+def MiniVgg_Adam():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(32,32))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_miniVgg.model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label  + " (MiniVgg-Adam)"
+
+
+@app.route('/MiniVgg-Nadam', methods=['POST']) 
+def MiniVgg_Nadam():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(32,32))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_miniVgg(NADAM).model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label+ " (MiniVgg-Nadam)"   
+
+
+@app.route('/MiniVgg-SGD', methods=['POST']) 
+def MiniVgg_SGD():
+    
+    #Setting for using CPU
+    tf.config.experimental.set_visible_devices([], 'GPU')
+    tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.list_logical_devices('GPU')
+
+    url = request.get_json('imgBs64')
+    stri1= str(url)[0:].split(" ")[1]
+    stri2= stri1[0:].split("'")[1]
+    stri3= stri2[0:].split("'")[0]
+    img = imread(io.BytesIO(base64.b64decode(stri3)))
+    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # print(cv2_img)
+    path = "E:/Kuliah/TA(Program)/API/Gambar_Send"
+    nama = str(uuid.uuid4())+".jpg"
+    cv2.imwrite(os.path.join(path, nama), cv2_img)
+    pathimage = "E:/Kuliah/TA(Program)/API/Gambar_Send/"+nama
+    print(nama)
+    image = cv2.imread(pathimage, cv2.IMREAD_COLOR)
+    orig = image.copy()
+   
+    #prepocessing histogram eq rgb
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(image)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    image = cv2.merge((y_eq, cr, cb))
+    image = cv2.cvtColor(image, cv2.COLOR_YCR_CB2BGR)
+
+    #resize
+    image = cv2.resize(image,(32,32))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis = 0)
+
+    #load model yang sudah dilatih
+    print("[Info] Loading Network.....")
+    # model = load_model("train_lenet.model.h5")
+    model = tf.keras.models.load_model("train_miniVgg(SGD).model.h5")
+    array = model.predict(image)
+    result = array[0]
+    answer = np.argmax(result)
+
+    if answer == 0:
+        label = "Mentah"
+    elif answer == 1:
+        label = "Light Roast"
+    elif answer == 2:
+        label = "Medium Roast"
+    elif answer == 3:
+        label = "Dark Roast"
+
+    label = "{}".format(label)
+
+    print("hasil prediksi : " + label)
+    return label+ " (MiniVgg-SGD)"
 
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.1.7')
--- Roasting_Coffee_Apps/platforms/browser/www/js/takePhoto.js
+++ Roasting_Coffee_Apps/platforms/browser/www/js/takePhoto.js
@@ -13,36 +13,184 @@ let app = {
             targetWidth: 400,
             targetHeight: 300
         };
-        
         navigator.camera.getPicture(app.ftw, app.wta, opts);
     },
 
     //jika sukses
     ftw: function(imgURI){
-        //  alert(imgURI);
-        sessionStorage.setItem('image', imgURI);
-        // document.getElementById('msg').textContent = imgURI;
         //konvert base64 string ke image
         document.getElementById('photo').src = "data:image/jpeg;base64," +imgURI;
         console.log(imgURI);
-        // sessionStorage.setItem("img",imgURI);
-        $.ajax({
-            url: "http://192.168.1.7:5000//LeNet-Adam",
-            type: "POST",
-            datatype: "json",
-            crossDomain: true,
-            data: JSON.stringify({ imgBs64:imgURI }),
-            cache: false,
-            processData: false,
-
-            success: function(result){
-                console.log("berhasil predict");
-                console.log(result)
-                var hasil = result;
-                document.getElementById('box-hasil').style.display = "block";
-                document.getElementById('msg').innerHTML = hasil;
+        var op = document.getElementById('select-box1');
+        var opsinilai =  op.options[op.selectedIndex].value;
+        console.log(opsinilai);
+        if(opsinilai!=null){
+            if(opsinilai == "LeAdam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//LeNet-Adam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "LeNadam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//LeNet-Nadam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai=="LeSGD"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//LeNet-SGD",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "AleAdam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//AlexNet-Adam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "AleNadam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//AlexNet-Nadam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "AleSGD"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//AlexNet-SGD",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "MinAdam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//MiniVgg-Adam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "MinNadam"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//MiniVgg-Nadam",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
+            } else if(opsinilai == "MinSGD"){
+                $.ajax({
+                    url: "http://192.168.1.7:5000//MiniVgg-SGD",
+                    type: "POST",
+                    datatype: "json",
+                    crossDomain: true,
+                    data: JSON.stringify({ imgBs64:imgURI }),
+                    cache: false,
+                    processData: false,
+        
+                    success: function(result){
+                        console.log("berhasil predict");
+                        console.log(result)
+                        var hasil = result;
+                        document.getElementById('box-hasil').style.display = "block";
+                        document.getElementById('msg').innerHTML = hasil;
+                    }
+                });
             }
-        });
+        } else{
+            alert("Pilih Arsitektur Dan Optimasi Yang Digunakan")
+        }
     },
 
     //jika gagal
