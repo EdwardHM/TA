5cb0b83c05fdabd2312042f1b077a3353d9d2e13
Edward H.M <edwardhm865@gmail.com>
2020-06-18 01:18:04 +0700

    sudah bisa pindahkan gambar

--- APIdanAdmin/Tambah_data.py
+++ APIdanAdmin/Tambah_data.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template     
+from flask import Flask, render_template, request   
 from imutils import paths 
+import json
 import os
+import cv2
+from imageio import imread
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+CORS(app)
 
 @app.route("/")
 def home():
@@ -10,9 +17,35 @@ def home():
     # imagePaths = sorted(list(paths.list_images("../prepo")))
     return render_template("Home.html", images=images)
     
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/Move", methods=["GET", "POST"])
+def Move():
+    data = request.get_json('gambar')
+    namaAwal = str(data["gambar"])
+    # kelas = request.get_json('kls')
+    kelas = str(data["kls"])
+    path = "E:/Kuliah/TA(Program)/APIdanAdmin/static/Gambar_Send/"+namaAwal
+    pindah = "E:/Kuliah/TA(Program)/ujiPindah"
+    img_data = cv2.imread(path, cv2.IMREAD_COLOR)
+    # histogram equalization RGB
+    img_y_cr_cb = cv2.cvtColor(img_data, cv2.COLOR_BGR2YCrCb)
+    y, cr, cb = cv2.split(img_y_cr_cb)
+
+    # equalize Hist operation di Y channel.
+    y_eq = cv2.equalizeHist(y)
+
+    img_y_cr_cb_eq = cv2.merge((y_eq, cr, cb))
+    img_rgb_eq = cv2.cvtColor(img_y_cr_cb_eq, cv2.COLOR_YCR_CB2BGR)
+
+    #simpan ke prepo
+    #ambil nama asli file e
+    savename = kelas+".jpg"
+    cv2.imwrite(os.path.join(pindah, savename),img_rgb_eq)
+    # cv2.imshow('Test image',image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    response = "Response: Gambar: " + namaAwal + " Menjadi: "+kelas
+    return response
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
+    app.run(host='192.168.1.6', port=5001)
--- APIdanAdmin/static/js/Function.js
+++ APIdanAdmin/static/js/Function.js
@@ -1,9 +1,23 @@
-function Ambildata(){
-    $('.AmbilData').click(function (event) {
-        var $row = $(this).parents('tr');
-        var desc = $row.find('input[name="kelas"]').val();
-        var nama = $row.find('input[name="name"]').text;
-    
-        alert('description: ' + desc + 'nama '+nama);
+function  getId(element) {
+;
+    // var kolom = element.parentNode.cellIndex;
+    var baris = element.parentNode.parentNode.rowIndex;
+    var nama = document.getElementById("ListFoto").rows[baris].cells.item(3).innerHTML;
+    var kelas = document.getElementById("ListFoto").rows[baris].cells[1].childNodes[1].value;
+    // alert("row" + element.parentNode.parentNode.rowIndex + 
+    // " - nama" + nama + " kelas" + kelas);
+    $.ajax({
+        url: "http://192.168.1.6:5001/Move",
+        type: "POST",
+        datatype: "json",
+        crossDomain: true,
+        data:JSON.stringify( { gambar:nama, kls:kelas} ),
+        cache: false,
+        processData: false,
+
+        success: function(result){
+            console.log("berhasil pindah");
+            alert(result)
+        }
     });
 }
